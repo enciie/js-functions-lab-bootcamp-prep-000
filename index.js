5adfe56c6c27044145e2296eef90b98da05056d8
Nancy Jo Chang <enciie@gmail.com>
2018-02-22 00:05:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(holiday, days, )
+function holidayCountdown(holiday, days){
+  return 
+}
